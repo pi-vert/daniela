cdaf8a7f3ece83da237aaff846af52b7736359ae
pi-vert <37361508+pi-vert@users.noreply.github.com>
2023-10-29 20:18:06 +0100

    Create servo.py

---
+++ servo.py
@@ -0,0 +1,33 @@
+#Libraries
+import sys
+import time    #https://docs.python.org/fr/3/library/time.html
+from adafruit_servokit import ServoKit    #https://circuitpython.readthedocs.io/projects/servokit/en/latest/
+#Constants
+nbPCAServo=16
+#Parameters
+MIN_IMP  =[500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500]
+MAX_IMP  =[2500, 2500, 2500, 2500, 2500, 2500, 2500, 2500, 2500, 2500, 2500, 2500, 2500, 2500, 2500, 2500]
+MIN_ANG  =[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+MAX_ANG  =[180, 180, 180, 180, 180, 180, 180, 180, 180, 180, 180, 180, 180, 180, 180, 180]
+#Objects pca=ServoKit(channels=16, address=40)
+pca = ServoKit(channels=16)
+
+# function init
+def init():
+    for i in range(nbPCAServo):
+        pca.servo[i].set_pulse_width_range(MIN_IMP[i] , MAX_IMP[i])
+
+# function main
+def main():
+    pcaMove(int(sys.argv[1]),int(sys.argv[2]));
+
+# function pcaScenario
+def pcaMove(motor,angle):
+    if (angle >= MIN_ANG[motor] and angle <= MAX_ANG[motor]):
+        pca.servo[motor].angle = angle
+    else:
+        pca.servo[motor].angle=None #disable channel
+
+if __name__ == '__main__':
+    init()
+    main()
