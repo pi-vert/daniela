efce7fc786759264a37be061b4431b08ba6485fc
pi-vert <37361508+pi-vert@users.noreply.github.com>
2023-11-01 15:26:57 +0100

    Update daniela.py

--- daniela.py
+++ daniela.py
@@ -1,4 +1,5 @@
 #Libraries
+import sys
 import time    #https://docs.python.org/fr/3/library/time.html
 from adafruit_servokit import ServoKit    #https://circuitpython.readthedocs.io/projects/servokit/en/latest/
 #Constants
@@ -10,27 +11,28 @@ MIN_ANG  =[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 MAX_ANG  =[180, 180, 180, 180, 180, 180, 180, 180, 180, 180, 180, 180, 180, 180, 180, 180]
 #Objects pca=ServoKit(channels=16, address=40)
 pca = ServoKit(channels=16)
+
 # function init
 def init():
     for i in range(nbPCAServo):
         pca.servo[i].set_pulse_width_range(MIN_IMP[i] , MAX_IMP[i])
+
 # function main
 def main():
-    pcaScenario();
+    pcaMove(int(sys.argv[1]),int(sys.argv[2]),int(sys.argv[3]));
+
 # function pcaScenario
-def pcaScenario():
-    """Scenario to test servo"""
-    for i in range(nbPCAServo):
-        for j in range(MIN_ANG[i],MAX_ANG[i],1):
-            print("Send angle {} to Servo {}".format(j,i))
-            pca.servo[i].angle = j
-            time.sleep(0.01)
-        for j in range(MAX_ANG[i],MIN_ANG[i],-1):
-            print("Send angle {} to Servo {}".format(j,i))
-            pca.servo[i].angle = j
-            time.sleep(0.01)
-        pca.servo[i].angle=None #disable channel
-        time.sleep(0.5)
+def pcaMove(motor,angle1,angle2):
+    angle=angle1;
+    while angle < angle2:
+        pca.servo[motor].angle = angle
+        angle = angle + 1
+        time.sleep(0.05)
+    while angle > angle1:
+        pca.servo[motor].angle = angle
+        angle = angle - 1
+        time.sleep(0.05)
+
 if __name__ == '__main__':
     init()
     main()
