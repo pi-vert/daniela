1466757c4aeb13f06b5f9788ced51b10665672de
pi-vert <37361508+pi-vert@users.noreply.github.com>
2023-11-01 15:36:06 +0100

    Update daniela.py

--- daniela.py
+++ daniela.py
@@ -12,17 +12,8 @@ MAX_ANG  =[180, 180, 180, 180, 180, 180, 180, 180, 180, 180, 180, 180, 180, 180,
 #Objects pca=ServoKit(channels=16, address=40)
 pca = ServoKit(channels=16)
 
-# function init
-def init():
-    for i in range(nbPCAServo):
-        pca.servo[i].set_pulse_width_range(MIN_IMP[i] , MAX_IMP[i])
-
-# function main
-def main():
-    pcaMove(int(sys.argv[1]),int(sys.argv[2]),int(sys.argv[3]));
-
 # Set
-def pcaMove(motor,angle):
+def pcaSet(motor,angle):
     if (angle >= MIN_ANG[motor] and angle <= MAX_ANG[motor]):
         pca.servo[motor].angle = angle
     else:
@@ -30,15 +21,33 @@ def pcaMove(motor,angle):
         
 # Deplacement
 def pcaMove(motor,angle1,angle2):
-    angle=angle1;
-    while angle < angle2:
-        pca.servo[motor].angle = angle
-        angle = angle + 1
-        time.sleep(0.05)
-    while angle > angle1:
-        pca.servo[motor].angle = angle
-        angle = angle - 1
-        time.sleep(0.05)
+    if (angle1 < angle2):
+        angle = angle1
+        while angle < angle2:
+            pca.servo[motor].angle = angle
+            angle = angle + 1
+            time.sleep(0.05)
+    else:
+        angle = angle2
+        while angle > angle1:
+            pca.servo[motor].angle = angle
+            angle = angle - 1
+            time.sleep(0.05)
+
+# function init
+def init():
+    for i in range(nbPCAServo):
+        pca.servo[i].set_pulse_width_range(MIN_IMP[i] , MAX_IMP[i])
+    pcaSet(0,90);
+    pcaSet(1,90);
+    pcaSet(2,70);
+    pcaSet(3,90);
+    
+# function main
+def main():
+    while true:
+        pcaMove(2,70,120);
+        pcaMove(2,120,70);
 
 if __name__ == '__main__':
     init()
